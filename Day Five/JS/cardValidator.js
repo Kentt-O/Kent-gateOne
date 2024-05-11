61bbdfd07faa7647155f8f6bc17e3ac7fd142e77
Kentt-O <kentc.okoro@gmail.com>
2024-05-11 18:37:33 +0100

    Day five

--- Day Five/JS/cardValidator.js
+++ Day Five/JS/cardValidator.js
@@ -1,14 +1,12 @@
 "use strict"
 function cardValidator(cardDigits){
-		cardDigits = cardDigits.replace('-', '')
-		if cardDigits !=16:
-			return "Invalid card number"
-		else if cardDigits[0] != '4' and cardDigits[0] != '5' and cardDigits[0] != '6' :
-			return "Invalid card number"
-		else:
-			return "Valid"
-
-	}
-
+	cardDigits = cardDigits.replace('-', '')
+	if (cardDigits.length !== 16) {
+        	return "Invalid card number";
+   	} else if (cardDigits[0] !== '4' && cardDigits[0] !== '5' && cardDigits[0] !== '6') {
+        	return "Invalid card number";
+    	} else {
+        	return "Valid";
+    	}
 }
 console.log(cardValidator("4234-5678-9012-3456"))
