13875884b1e969dabbe7adf9e9a38cd9a3033f5e
Kentt-O <kentc.okoro@gmail.com>
2024-05-15 19:07:35 +0100

    PhoneBook

--- Day Seven/phoneBook/Java/PhoneBook.java
+++ Day Seven/phoneBook/Java/PhoneBook.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PhoneBook {
 	public static void  main (String... args){
 		Scanner input = new Scanner(System.in);
 
+    		static ArrayList<String> firstNameStorage = new ArrayList<>();
+    		static ArrayList<String> lastNameStorage = new ArrayList<>();
+    		static ArrayList<String> phoneNumberStorage = new ArrayList<>();
+ 		static ArrayList<String> emailStorage = new ArrayList<>();
 
-		Object[][] storage = new Object [4][4];
-		
-		storage [0] = new  Object [ ] {"firstName","lastName" ,phoneNumber,"email"};
-		storage [1] = new  Object [ ] {"firstName","lastName" ,phoneNumber,"email"};
-		storage [2] = new  Object [ ] {"firstName","lastName" ,phoneNumber,"email"};
-		storage [3] = new  Object [ ] {"firstName","lastName" ,phoneNumber,"email"};
 
-		
 		
 		String prompt = """
-		:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+		System.out.println(":".repeat(26));
 		:: Welcome to your new phonebook ::
-		:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+		System.out.println(": ".repeat(26));
 		Select an option 
 		1 -> Add Contact
 		2 -> Remove Contact
@@ -28,20 +27,20 @@ public class PhoneBook {
 		0 -> Back
 		""";
 
-		System.out.println(prompt)
+		System.out.println(prompt);
 		int userResponse = input.nextInt();
 
 		switch(userResponse){
 			case 1: {
-				System.out.println(addContact)
+				System.out.println(addContact());
 				break;
 			}
 			case 2: {
-				System.out.println(addContact)
+				System.out.println(removeContact());
 				break;
 			}
 			case 3: {
-				System.out.println(addContact)
+				System.out.println(findContactByPhoneNumber());
 				break;
 			}
 
@@ -50,21 +49,34 @@ public class PhoneBook {
 
 	}
 	public static void addContact(){
-		System.out.println("Enter first name");
-		String firstName = nextLine();
+		System.out.println("\nEnter first name: ");
+		String firstName = input.nextLine();
+		firstNameStorage.add(firstName); 
 
-		System.out.println("Enter last name");
-		String lastName = nextLine();
-		
-		System.out.println("Enter phonenumber");
-		String phoneNumber = nextLine();
+		System.out.println("\nEnter last name: ");
+		String lastName = input.nextLine();
+		lastNameStorage.add(lastName); 
 
-		System.out.println("Enter email");
-		String email = nextLine();
+		System.out.println("\nEnter phonenumber: ");
+		String phoneNumber = input.nextLine();
+		phoneNumberStorage.add(phoneNumber);
+
+		System.out.println("\nEnter email: ");
+		String email = input.nextLine();
+		emailStorage.add(email);
 
 	}
 
 	public static void removeContact(){
+		System.out.print("Enter name you want to remove: ");
+		String removeName = input.nextLine();
 
 	}
+
+	public static void findContactByPhoneNumber(){
+		System.out.print("Search phone number: ");
+		String phoneNumberSearch = input.nextLine();
+
+	}
+
 }
