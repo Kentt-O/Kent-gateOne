7ff5dc7266be27692d667134e095970a5f94492a
Kentt-O <kentc.okoro@gmail.com>
2024-05-15 11:54:08 +0100

    PhoneBook

---
+++ Day Seven/phoneBook/Java/PhoneBook.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PhoneBook {
+	public static void  main (String... args){
+		Scanner input = new Scanner(System.in);
+		
+		String prompt
+
+
+	}
+
+
+
+
+}
